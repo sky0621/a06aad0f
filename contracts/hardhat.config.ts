ce6c91e54ebd2ebd34009662acda73ee0437398c
sky0621 <sky14work@gmail.com>
2023-11-25 17:33:41 +0900

    -

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,18 +1,21 @@
 import {HardhatUserConfig, task} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"
 
-const INFURA_API_KEY = ""
-const SEPOLIA_PRIVATE_KEY = ""
+dotenv.config()
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY ?? ""
+const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY ?? ""
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   etherscan: {
-    apiKey: ""
+    apiKey: process.env.ETHERSCAN_API_KEY ?? ""
   },
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      accounts: [OWNER_PRIVATE_KEY]
     }
   }
 };
